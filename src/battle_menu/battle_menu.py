fde9f99a9c465ef2883c25e771186ff8b9d0b363
Chris Winikka <3827026+HundredVisionsGuy@users.noreply.github.com>
2026-01-16 09:16:34 -0800

    adding main file and updating readme

---
+++ src/battle_menu/battle_menu.py
@@ -0,0 +1,13 @@
+"""
+battle_menu.py
+by Chris Winikka
+A menu of options for a battle sequence in an RPG with various random outcomes
+"""
+
+
+def main():
+    print("Welcome")
+
+
+if __name__ == "__main__":
+    main()
